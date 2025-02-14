d54d716154a43444f98be287f0093bdf9e93f37f
FitnessFreakCoder <acerpredator8973@gmail.com>
2025-02-14 03:02:53 -0800

    Changed

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const updateFlag = (element) => {
   img.src = newSrc;
 };
 
-const URL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurr.value}`;
+const URL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurr.value}.json`;
 
 button.addEventListener("click", async (evt) => {
   evt.preventDefault();
